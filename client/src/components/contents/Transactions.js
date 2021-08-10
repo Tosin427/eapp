ec954c6e3bbb4a0478918ffb655258ed3532e9fe
Oluwatosin Ajiboye <48648548+Tosin427@users.noreply.github.com>
2021-08-10 19:20:20 +0100

    Update Transactions.js

--- client/src/components/contents/Transactions.js
+++ client/src/components/contents/Transactions.js
@@ -1,67 +1,90 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 // import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
 // import "./index.css";
 import { Table } from "antd";
 
-const columns = [
-  {
-    title: "Date",
-    width: 100,
-    dataIndex: "date",
-    key: "date",
-    fixed: "left",
-  },
-  {
-    title: "Type",
-    width: 100,
-    dataIndex: "age",
-    key: "age",
-    fixed: "left",
-  },
-  {
-    title: "Currency",
-    dataIndex: "address",
-    key: "1",
-    width: 150,
-  },
-  {
-    title: "Mode",
-    dataIndex: "address",
-    key: "2",
-    width: 150,
-  },
-  {
-    title: "Amount",
-    dataIndex: "address",
-    key: "3",
-    width: 150,
-  },
-  {
-    title: "Status",
-    dataIndex: "address",
-    key: "4",
-    width: 150,
-  },
-  {
-    title: "Details",
-    dataIndex: "address",
-    key: "5",
-    width: 150,
-  },
-];
-
-const data = [];
-for (let i = 0; i < 100; i++) {
-  data.push({
-    key: i,
-    date: `Edrward ${i}`,
-    age: 32,
-    address: `London Park no. ${i}`,
-  });
-}
-
 const Transactions = () => {
+  // console.log(getData[0].confirmed);
+  const columns = [
+    {
+      title: "Date",
+      width: 100,
+      dataIndex: "date",
+      key: "date",
+      fixed: "left",
+    },
+    {
+      title: "Type",
+      width: 100,
+      dataIndex: "age",
+      key: "age",
+      fixed: "left",
+    },
+    {
+      title: "Currency",
+      dataIndex: "address",
+      key: "1",
+      width: 150,
+    },
+    {
+      title: "Mode",
+      dataIndex: "address",
+      key: "2",
+      width: 150,
+    },
+    {
+      title: "Sent",
+      dataIndex: "sent",
+      key: "3",
+      width: 150,
+    },
+    {
+      title: "Status",
+      dataIndex: "address",
+      key: "4",
+      width: 150,
+    },
+    {
+      title: "Details",
+      dataIndex: "details",
+      key: "5",
+      width: 150,
+    },
+  ];
+
+  const data = [];
+
+  const [getData, setGetData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        // "https://blockchain.info/rawaddr/1HqUb1yWNgdbuvpbijz6FxRXzkGdQnmuZj"
+        `https://api.blockcypher.com/v1/btc/main/addrs/1HqUb1yWNgdbuvpbijz6FxRXzkGdQnmuZj`
+      )
+      .then((response) => setGetData(response.data.trefs))
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  // console.log(getData.data.txrefs[0]);
+
+  for (let i = 0; i < 5; i++) {
+    data.push({
+      key: i,
+      date: getData[i].value,
+      // sent: getData[i].value / 100000000,
+      // details: getData[1].tx_hash,
+      // age: 32,
+      // address: `London Park no. ${i}`,
+    });
+  }
+
+  buildData(getData);
+
   return (
     <div>
       <Table
